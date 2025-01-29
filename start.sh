cb3ff3bbef20416593079a0b447192b4863a6e97
iamkuug <jonathan.kuug@ic.africa>
2025-01-29 11:03:11 +0000

    add: glitch setup files

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-# Check if Python 3 is installed
-if ! command -v python3 &> /dev/null; then
-    echo "Python 3 not found. Installing..."
-    # For Debian/Ubuntu
-    apt-get update
-    apt-get install -y python3 python3-pip python3-venv
-fi
-
-
-python -m venv venv
-source venv/bin/activate
-
-pip install -r requirements.txt
-
-python src/app.py
+set -eux
+. /tmp/venv/bin/activate
+flask --app server run --port $PORT
