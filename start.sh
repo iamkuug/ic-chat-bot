a4de755cdd19a7036eb5e6b33c1f6f944c04ea5a
iamkuug <jonathan.kuug@ic.africa>
2025-01-29 10:27:39 +0000

    fix: python3 isntall start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 not found. Installing..."
+    # For Debian/Ubuntu
+    apt-get update
+    apt-get install -y python3 python3-pip python3-venv
+fi
+
+
 python -m venv venv
 source venv/bin/activate
 
