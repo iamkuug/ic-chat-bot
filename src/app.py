9a663189d6abd681399d7d9646cafa35e72f36e5
iamkuug <jonathan.kuug@ic.africa>
2025-02-10 14:51:50 +0000

    feat: migrate flask to fastAPI

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, Response
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
 from utils.mark_as_read import *
 from utils.profiler import *
 from utils.send_reply import *
@@ -10,12 +11,13 @@ from constants import WEBHOOK_VERIFY_TOKEN, OPENAI_ASSISTANT_ID
 from redis.asyncio import Redis
 from dotenv import load_dotenv
 import os
+import uvicorn
 
 load_dotenv()
 check_env_status()
 
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 redis_client = Redis(
@@ -28,7 +30,7 @@ redis_client = Redis(
 )
 
 
-@app.route("/", methods=["GET"])
+@app.get("/")
 async def health():
     try:
         await redis_client.ping()
@@ -38,7 +40,7 @@ async def health():
 
 
 @app.get("/webhook")
-def verify_webhook():
+def verify_webhook(request: Request):
     mode = request.args.get("hub.mode")
     token = request.args.get("hub.verify_token")
     challenge = request.args.get("hub.challenge")
@@ -52,9 +54,9 @@ def verify_webhook():
 
 
 @app.post("/webhook")
-async def webhook():
+async def webhook(request: Request):
     try:
-        body = request.json
+        body = await request.json()
         message = (
             body.get("entry", [{}])[0]
             .get("changes", [{}])[0]
@@ -63,10 +65,9 @@ async def webhook():
         )
 
         if message.get("type") != "text":
-            return Response("", 204)
+            return Response(content="", status_code=204)
 
         logger.debug(f"Incoming webhook message: {message}")
-
         user_message = message["text"]["body"]
         sender_phone_number = message["from"]
         message_id = message["id"]
@@ -88,7 +89,7 @@ async def webhook():
         message_body = gpt_reply.get("message")
         stock = gpt_reply.get("stock")
 
-        logger.debug(f"Incomming Message type: {message_type}")
+        logger.debug(f"Incoming Message type: {message_type}")
 
         if message_type == "GENERAL":
             await send_reply(sender_phone_number, message_body, message_id)
@@ -98,18 +99,17 @@ async def webhook():
                 sender_phone_number, message_body, message_type, stock
             )
             message_body = res.get("data").get("message")
-
             await send_reply(sender_phone_number, message_body, message_id)
             await mark_as_read(message_id)
 
-        return Response("success", 200)
+        return JSONResponse(content="success", status_code=200)
 
     except Exception as e:
         logger.error(f"Error processing webhook: {str(e)}")
-        return Response(str(e), 500)
+        return JSONResponse(content=str(e), status_code=500)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("PORT"))
-
-
+    uvicorn.run(
+        "app:app", host="127.0.0.1", port=int(os.getenv("PORT", 8000)), reload=True
+    )
